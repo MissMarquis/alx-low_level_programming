43d4624f11b9ef952f74c96180bdc6929306f1c8
MissMarquis <adejokemarquis@gmail.com>
2022-04-26 16:45:55 -0700

    a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints buffer in hexa
- * @a: cheese
- * needle: buffer2
- *
- * Return: Nothing.
+ * print_chessboard - prints the chessboard
+ * @a: input pointer.
+ * Return: no return.
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i, m;
 
-	for (i = 0; i < 8; i++)
+	i = 0;
+
+	while (i < 8)
 	{
-		for (j = 0; j < 8; j++)
+		m = 0;
+		while (m < 8)
 		{
-			_putchar (a[i][j]);
+			_putchar(a[i][m]);
+			m++
 		}
-	_putchar('\n');
-		}
+		_putchar('\n');
+		i++;
+	}
 }
